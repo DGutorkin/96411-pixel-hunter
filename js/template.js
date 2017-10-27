9fc03af8101c1106a776596e119f6157a7748100
Dmitry Gutorkin <dgutorkin@gmail.com>
2017-10-27 18:16:55 +0300

    Вова скажет "Усложняешь": getTemplate принимает дополнительно опциональные параметры tagName и classList

--- js/template.js
+++ js/template.js
@@ -1,6 +1,14 @@
-export default (htmlMarkup) => {
-  const element = document.createElement(`div`);
+/**
+* Функция, возвращающая элемент, на основе переданной разметки
+* @function getTemplate
+* @param {string} htmlMarkup - HTML-разметка, помещаемый внутрь элемента
+* @param {string} tagName - имя тэга, в который добавляется разметка
+* @param {Array} classList - опциональный набор классов для элемента
+* @return {HTMLElement}
+*/
+export default (htmlMarkup, tagName = `div`, classList = []) => {
+  const element = document.createElement(tagName);
+  element.classList.add(...classList);
   element.innerHTML = htmlMarkup;
-
   return element;
 };
